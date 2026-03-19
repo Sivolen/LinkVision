688773d870d06f90ccb008f147010cb24c8a1e94
Sivolen <anton@gridnev.pro>
2026-03-20 08:17:06 +1100

    v1.3.2 Hot fix bugs

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 from app import create_app
 from extensions import socketio
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     socketio.run(app, debug=True, use_reloader=False, port=8005, host="0.0.0.0", allow_unsafe_werkzeug=True)
