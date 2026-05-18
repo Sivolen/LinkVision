3d69bc7a47ef2fc066c9fc11603132c506a1a0a2
a.gridnev <a.gridnev@sakhalin.gov.ru>
2026-05-19 09:12:00 +1100

    feat: real-time map sync, viewport restore, DB & frontend optimizations

--- services/notifications.py
+++ services/notifications.py
@@ -3,4 +3,6 @@ from extensions import socketio
 
 def notify_map_updated(map_id, skip_sid=None):
     """Отправить всем в комнату карты (кроме skip_sid) сигнал об обновлении."""
-    socketio.emit('map_updated', {'map_id': map_id}, room=f'map_{map_id}', skip_sid=skip_sid)
+    socketio.emit(
+        "map_updated", {"map_id": map_id}, room=f"map_{map_id}", skip_sid=skip_sid
+    )
